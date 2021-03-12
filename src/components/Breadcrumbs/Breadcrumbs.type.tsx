3f5b689152f90a421af4a602aefbdc381e406904
Akepogu  Prakash <fc11034@superaxiss-MacBook-Pro.local>
2021-03-12 14:32:27 +0530

    Added Link Comp and Breadcrumbs partially

---
+++ src/components/Breadcrumbs/Breadcrumbs.type.tsx
@@ -0,0 +1,9 @@
+import { Ref } from 'react';
+import { BreadcrumbsProps as MuiBreadcrumbsProps, BreadcrumbsClassKey } from '@material-ui/core/Breadcrumbs';
+
+export interface BreadcrumbsStylingProps extends Partial<Record<BreadcrumbsClassKey, string>> {
+}
+
+export interface BreadcrumbsProps extends MuiBreadcrumbsProps {
+  innerRef: Ref<HTMLElement>;
+}
