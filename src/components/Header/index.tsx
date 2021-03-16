11ceb50ee25e67332d317858bc7109d5c5d9b884
Akepogu  Prakash <fc11034@superaxiss-MacBook-Pro.local>
2021-03-16 13:02:51 +0530

    added Header Comp

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import Header from './Header';
+import { HeaderProps } from './Header.type';
+
+export type { HeaderProps }
+
+export default Header;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as Link } from './Link';
 export { default as Breadcrumbs } from './Breadcrumbs';
+export { default as Header } from './Header';
