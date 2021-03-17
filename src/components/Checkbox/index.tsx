d6aac0e0ef8156e3c381a5d240bfe7f44b8fa11b
Akepogu  Prakash <fc11034@superaxiss-MacBook-Pro.local>
2021-03-17 23:44:47 +0530

    added checkbox comp

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,6 @@
+import Checkbox from './Checkbox';
+import { CheckboxProps } from './Checkbox.type';
+
+export type { CheckboxProps }
+
+export default Checkbox;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,9 @@ export { default as Breadcrumbs } from './Breadcrumbs';
 export { default as Header } from './Header';
 export { default as Divider } from './Divider';
 export { default as Chip } from './Chip';
-export { default as YesNoSwitch } from './YesNoSwitch';
 export { default as Skrim } from './Skrim';
+export { default as Checkbox } from './Checkbox';
+// export { default as CheckboxList } from './CheckboxList';
+
+//* NEED TODO FURTHER
+// export { default as YesNoSwitch } from './YesNoSwitch';
