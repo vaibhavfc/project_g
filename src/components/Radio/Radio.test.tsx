2aeabb248b879859afe8529b65c055e7f22859ee
Akepogu  Prakash <fc11034@superaxiss-MacBook-Pro.local>
2021-03-25 12:20:55 +0530

    radio test cases

--- src/components/Radio/Radio.test.tsx
+++ src/components/Radio/Radio.test.tsx
@@ -1,16 +1,31 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import renderer from 'react-test-renderer';
 
 import Radio from './Radio';
 
 describe('test', () => {
-  it('Renders Radio Compoents', () => {
-    render(<Radio>test</Radio>);
+  it('Should Render Radio Compoents', () => {
+    render(<Radio label="Radio Item" />);
   });
 
-  it('Matches Snapshot of Radio', () => {
-    const tree = renderer.create(<Radio> TEST</Radio>).toJSON();
+  it('Should match Snapshot of Radio', () => {
+    const tree = renderer.create(<Radio label="Radio Item" />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('Should render correct label', () => {
+    render(<Radio label="Radio Item" />);
+    expect(screen.getByText('Radio Item')).toBeTruthy();
+  });
+
+  it('Should be checked if checked flag is true', () => {
+    render(<Radio label="Radio Item" checked />);
+    expect(screen.getByRole('radio').checked).toBe(true);
+  });
+
+  it('Should be disabled if disabled flag is true', () => {
+    render(<Radio label="Radio Item" disabled checked />);
+    expect(screen.getByText('Radio Item').classList.contains('Mui-disabled')).toBe(true);
   });
 });
