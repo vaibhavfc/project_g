ca3a9da3afa3955c71e6d9fe35aeae303eeabdda
Aman  Thakur <fc19343@fc19l-mac043.local>
2021-04-19 19:54:35 +0530

    minor fixex

--- src/components/Toggle/Toggle.stories.tsx
+++ src/components/Toggle/Toggle.stories.tsx
@@ -19,6 +19,12 @@ const Template: Story<ToggleProps> = (args) => <Toggle {...args} />;
 
 export const DefaultAndToggle = Template.bind({});
 DefaultAndToggle.args = {
-  type: 'label',
-  buttonType: 'icon',
+  type: 'default',
+  buttonType: 'Selected',
+};
+
+export const DefaultAndUnSelected = Template.bind({});
+DefaultAndUnSelected.args = {
+  type: 'default',
+  buttonType: 'Unselected',
 };
